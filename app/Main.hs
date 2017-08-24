4964d30c5f26cf0cead89445b3e9b2ed40ab55e4
HaishengWu <freizl@gmail.com>
2017-08-24 11:36:12 -0700

    parse all links and try to fetch all datas but pages are still vary..darn..

--- app/Main.hs
+++ app/Main.hs
@@ -5,25 +5,37 @@ module Main where
 import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import           Lib
 import           Network.HTTP.Conduit
 import qualified Text.HTML.DOM as HTML
-import           Text.XML
+import           Text.XML hiding (writeFile)
+import Data.Either
+import Data.Aeson (encode)
+import qualified Data.ByteString.Lazy as BS
+import Control.Concurrent.Async
+import System.IO
+import Control.Monad
+
+import           Lib
 
 main :: IO ()
 main = do
   mgr <- newManager tlsManagerSettings
   resp <- makeReq mgr mainPage
-  mapM_ (fetchFillingDate mgr) (bulletinMain resp)
+  xs <- mapConcurrently (fetchFillingDate mgr) (take 20 $ bulletinMain resp)
+  -- TODO merge errors
+  let rs = rights xs
+  let ls = lefts xs
+  print $ length xs
+  print $ length rs
+  unless (null ls) (writeFile "./error.log" (show ls))
+  BS.writeFile "./data.json" (encode rs)
 
-fetchFillingDate :: Manager -> Either ErrorResult BulletinLink -> IO ()
-fetchFillingDate _ (Left e) = print e
+fetchFillingDate :: Manager -> Either ErrorResult BulletinLink -> IO (Either [ErrorResult] Bulletin)
+fetchFillingDate _ (Left e) = return $ Left [e]
 fetchFillingDate mgr (Right r) = do
   T.putStrLn (title r)
   resp <- makeReq mgr (url r)
-  --print resp
-  let rs = bulletinDetail resp
-  mapM_ (either print print) rs
+  return $ bulletinDetail resp
 
 
 makeReq :: Manager -> Text -> IO Document
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Lib where
 
@@ -6,8 +7,12 @@ import           Control.Monad
 import           Data.Text                  (Text)
 import qualified Data.Text                  as T
 import qualified Data.Text.Encoding                  as T
+import qualified Data.Text.Read                  as T
 import           Text.XML
 import           Text.XML.Cursor
+import Data.Either
+import Data.Aeson
+import GHC.Generics
 
 type URL = String
 
@@ -19,15 +24,31 @@ mainPage = baseURL `T.append` "/content/visas/en/law-and-policy/bulletin.html"
 
 data ErrorResult = ErrorResult { msg :: Text
                                , lastCursor :: [Cursor]
-                               } deriving (Show)
-{-
+                               } deriving (Show, Eq, Generic)
 
-data Bulletin = Bulletin { title :: Text
-                         , url :: Text
-                         , ebType :: Text
-                         , date :: Text
-                         } deriving (Show)
--}
+data VisaType = EB2 | EB3
+  deriving (Show, Eq, Generic)
+
+data VisaDateType = FinalAction | Filing
+  deriving (Show, Eq, Generic)
+
+data Bulletin = Bulletin
+  { year :: Int
+  , month :: Text
+  , bulletins :: [BulletinNode]
+  } deriving (Show, Eq, Generic)
+
+data BulletinNode = BulletinNode {
+  visaDateType :: VisaDateType
+  , visaType :: Maybe VisaType
+  , bulletDate   :: Text
+  } deriving (Show, Eq, Generic)
+
+instance ToJSON ErrorResult
+instance ToJSON VisaType
+instance ToJSON VisaDateType
+instance ToJSON Bulletin
+instance ToJSON BulletinNode
 
 data BulletinLink = BulletinLink { title :: Text
                                  , url   :: Text
@@ -38,10 +59,22 @@ data BulletinDate = BulletinDate { ebType :: Text
                                  }
 
 instance Show BulletinDate where
-  show (BulletinDate t d) = T.unpack ("\t" `T.append` t `T.append` ": " `T.append` d)
+  show (BulletinDate t d) = "\t" ++ show t ++ ": " ++ T.unpack d
 
 bulletinMain :: Document -> [Either ErrorResult BulletinLink]
-bulletinMain doc = fromDocument doc $// (findCurrentLi &// findBulletLink &| genBulletinLink)
+bulletinMain doc = fromDocument doc
+  $// (element "dl" >=> attributeIs "class" "data_show_hide"
+       &// element "ul" >=> attributeIs "class" "default"
+       &/ element "li"
+       &/ element "p"
+       &/ element "a"
+       >=> check isVisaBulletinLink
+       &| genBulletinLink)
+
+isVisaBulletinLink :: Cursor -> Bool
+isVisaBulletinLink c = let t' = attribute "title" c
+                       in
+                         length t' == 1 && "Visa Bulletin" `T.isPrefixOf` head t'
 
 findCurrentLi :: Cursor -> [Cursor]
 findCurrentLi = element "li" >=> attributeIs "class" "current"
@@ -57,27 +90,61 @@ genBulletinLink c = let t' = attribute "title" c
                       then Right (BulletinLink (head t') (baseURL `T.append` head u'))
                       else Left (ErrorResult "Cant find bulletin link" [c])
 
-bulletinDetail :: Document -> [Either ErrorResult BulletinDate]
+bulletinDetail :: Document -> Either [ErrorResult] Bulletin
 bulletinDetail doc =
   let c = fromDocument doc
       n1 = c $// categoryDiv &/ sectionDiv
-      n2 = filter isEmploymentBased n1
+      titleNode = c $// pageTitleTextNode
+      ym = if length titleNode == 1
+        then getYearAndMonth (head titleNode)
+        else Left (ErrorResult "No title found for the page" titleNode)
   in
-    if length n2 == 1
-    then genBulletinDate (head n2)
-    else [Left (ErrorResult ("Cant find EB date for filling table = "
-                             `T.append`
-                             T.pack ((show $ length n1) ++ (show $ length n2)) ) n1)]
+    if length n1 /= 5
+    then Left [ ErrorResult (T.pack $ "Expect 5 tables but gets " ++ show (length n1) ) n1 ]
+         -- 4th one is EB final action date
+         -- 5th one is EB filling date
+    else let bnodes = map ((Right . toBullet FinalAction) =<<) (genBulletinDate (n1 !! 3))
+               ++ map ((Right . toBullet Filing) =<<) (genBulletinDate (n1 !! 4))
+             brights = rights bnodes
+             blefts = lefts bnodes
+         in
+           case ym of
+             Left l -> Left (l : blefts)
+             Right (y, m) -> Right (Bulletin y m brights)
+
+getYearAndMonth :: Cursor    -- page title, "Visa Bulletin For November 2016"
+                -> Either ErrorResult (Int, Text)
+getYearAndMonth tc = let ts = T.splitOn " " (head $ content tc)
+                    in
+                      if length ts == 5
+                      then case T.decimal (ts !! 4) of
+                        Right (i, _) -> Right (i, ts !! 3)
+                        Left _ -> Left (ErrorResult "Cant parse year from title" [tc])
+                      else Left (ErrorResult "Cant parse title" [tc])
+
+toBullet :: VisaDateType -> BulletinDate -> BulletinNode
+toBullet vdt (BulletinDate ebt nodeDate) =
+  BulletinNode { visaDateType = vdt
+               , visaType = vt
+               , bulletDate = nodeDate
+               }
+  where vt = toEBVisaType ebt
+
+toEBVisaType :: Text -> Maybe VisaType
+toEBVisaType s
+  | s == "2nd" = Just EB2
+  | s == "3rd" = Just EB3
+  | otherwise = Nothing
 
 
-genBulletinDate :: Cursor   -- section div
+genBulletinDate :: Cursor   -- section DIV
                 -> [Either ErrorResult BulletinDate]
 genBulletinDate = map dateTd . concatMap dateTr . firstTable
 
 firstTable :: Cursor -> [Cursor]
 firstTable = take 1 . ($// element "table")
 
-dateTr :: Cursor          -- Table
+dateTr :: Cursor          -- TABLE
        -> [Cursor]
 dateTr c = take 2 $ drop 2 (c $// element "tr")
 
@@ -97,6 +164,11 @@ categoryDiv = element "div" >=> attributeIs "class" "Visa_Contentpage_Category p
 sectionDiv :: Cursor -> [Cursor]
 sectionDiv = element "div" >=> attributeIs "class" "simple_richtextarea section"
 
+pageTitleTextNode :: Cursor -> [Cursor]
+pageTitleTextNode c = c $// element "title" >=> child
+
+{-
+
 uTextNode :: Cursor -> [Cursor]
 uTextNode c = c $// element "u" >=> child
 
@@ -105,3 +177,5 @@ isEmploymentBased = (== 1) . length . filter isEmploymentBasedContent . uTextNod
 
 isEmploymentBasedContent :: Cursor -> Bool
 isEmploymentBasedContent = (== ["DATES FOR FILING OF EMPLOYMENT-BASED"]) . content
+
+-}
